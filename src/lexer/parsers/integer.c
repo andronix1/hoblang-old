ecb1371004c1f169b3e0a28325f3d2bf690092c1
andronix1 <andronix1.dev@gmail.com>
2025-01-24 10:49:04 +0700

    http example

--- src/lexer/parsers/integer.c
+++ src/lexer/parsers/integer.c
@@ -5,10 +5,20 @@ LexOneErr lex_integer(Lexer *lexer) {
 	if (!char_is_digit(c = lexer_next_char(lexer))) {
 		return LEX_ONE_MISSMATCH;
 	}
-	size_t result = c - '0';
-	while (char_is_digit(c = lexer_future_char(lexer))) {
+	int base = 10;
+	if (c == '0' && lexer_future_char(lexer) == 'x') {
+		base = 16;
 		lexer_next_char(lexer);
-		result = result * 10 + c - '0';
+		if (!char_is_digit(c = lexer_next_char(lexer))) {
+			lex_err("expected digit after 16 bit value prefix");
+			return LEX_ONE_ERR;
+		}
+	}
+	uint64_t result = c - '0';
+	while (char_is_digit(c = lexer_future_char(lexer))) {
+		if (base == 16) printf("%c\n", c);
+		lexer_next_char(lexer);
+		result = result * base + c - '0';
 	}
 	lexer->token.type = TOKEN_INTEGER;
 	lexer->token.integer = result;
